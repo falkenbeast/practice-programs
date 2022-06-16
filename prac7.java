507b96de3ddfea6fd77b8d40ae5b844ef04c9ce9
falkenbeast <arshayush21@gmail.com>
2022-06-16 11:37:49 +0530

    init

---
+++ prac7.java
@@ -0,0 +1,8 @@
+public class prac7 {
+        public static void main(String []args)
+        {
+                String s = "ayush srivastava";
+                boolean b = s.endsWith("h");
+                System.out.println(b);
+        }
+}
---
+++ prac8.java
@@ -0,0 +1,9 @@
+class prac8
+{
+        public static void main(String []args)
+        {
+                String a = "    java exercise 1     ";;
+                String aa = a.trim();
+                System.out.println(aa);
+        }
+}
