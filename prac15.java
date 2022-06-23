f8b0682951b3a3ed0968f3fd0be0b94e74baea15
falkenbeast <arshayush21@gmail.com>
2022-06-24 00:09:46 +0530

    c

--- prac15.java
+++ prac15.java
@@ -1,11 +1,13 @@
 package practice;
 import programs.progprac.*;
-class prac15
+public class prac15
 {
     public static void main(String []args)
     {
         accessm A = new accessm();
         System.out.println(A.s);
+        accessm2 a = new accessm2();
+        a.main(args);
     }
 }
 
